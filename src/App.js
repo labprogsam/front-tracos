fb4617f74a1518c235c3f95abce38a8607a5175b
SamuelMiranda-neuro <samuel.miranda@neurotech.com.br>
2025-03-12 22:29:27 -0300

    Fix: Remove comment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,3 @@
-// import React from 'react';
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import { Base } from './components';
-// import { Login, Register } from './views';
-
-// const AppRoutes = () => {
-  
-//   return (
-//     <Router>
-//       <Base>
-//         <Routes>
-//           <Route path="/login" element={<Register />}/>
-//           <Route path="/register" element={<Register />}/>
-//           <Route path="/" element={<div>aaaaaaaaaaa</div>}/>
-//         </Routes>
-//       </Base>
-//     </Router>
-//   );
-// };
-
-// export default AppRoutes;
-
 import React from "react";
 import "./App.css";
 import InternalRoutes from "./routes/InternalRoutes";
